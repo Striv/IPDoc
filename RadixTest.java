f64b6f817c322e6fa3a8bc582cfa0365b2ec47f1
Striv <strivcini@gmail.com>
2014-04-10 23:17:03 -0400

    Parsing sortof works, NEED: validate ips and add counter

--- RadixTest.java
+++ RadixTest.java
@@ -25,26 +25,40 @@ public class RadixTest {
         ArrayList<String> ipList = new ArrayList<String>();
         String in = fileContents;
         //--------------------------------------------
-        // Binary / Hexadecimal / Octal Strings (Dot Separated)
-        String re1 = "([0-9a-fxA-FX]{1,8})\\.([0-9a-fxA-FX]{1,8})\\.([0-9a-fxA-FX]{1,8})\\.([0-9a-fxA-FX]{1,8})";
+        // 
+        // Decimal Strings (Dot Separated)
+        String re4 = "([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})";
+        // Hex Strings (Dot Separated)
+        String re1 = "0x([0-9a-fA-F]{1,2})\\.0x([0-9a-fA-F]{1,2})\\.0x([0-9a-fA-F]{1,2})\\.0x([0-9a-fA-F]{1,2})";
+        // Binary String (Dot Separated)
+        String re2 = "([0-1]{7,8})\\.([0-1]{7,8})\\.([0-1]{7,8})\\.([0-1]{7,8})";
+        // Octal String (Dot Separated)
+        String re3 = "0[0-7]{3}\\.0[0-7]{3}\\.0[0-7]{3}\\.0[0-7]{3}";
+
         // Binary Concatenated
-        String re2 = "([0-1]{31,32})";
+        String re5 = "([0-1]{31,32})";
         // Hexadecimal String Concatenated
-        String re3 = "0x([0-9a-fA-F]{7,8})";
+        String re6 = "0x([0-9a-fA-F]{7,8})";
         // Octal String Concatenated
-        String re4 = "([0-7]{11,12})";
-        
-        Pattern p = Pattern.compile(re,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-        Matcher m = p.matcher(in);
-        
-        if (m.find())
+        String re7 = "([0-7]{11,12})";
+
+        String [] rex = {re1, re2, re3, re4}; //, re3, re4, re5};
+        while(!"".equals(in))
         {
-            System.out.println(in.substring(m.start(),m.end()));
-            String int1=m.group(1);
-            String int2=m.group(2);
-            String int3=m.group(3);
-            String int4=m.group(4);
-            System.out.print("("+int1.toString()+")"+"("+int2.toString()+")"+"("+int3.toString()+")"+"("+int4.toString()+")"+"\n");
+            for(String re : rex)
+            {
+                Pattern p = Pattern.compile(re,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+                Matcher m = p.matcher(in);
+            
+                if (m.find())
+                {
+                    System.out.println(in.substring(m.start(),m.end()));
+                    ipList.add(makeIP(in.substring(m.start(),m.end())));
+                    in = in.substring(m.end()+1);
+                }
+                else
+                    in = "";
+            }
         }
         //--------------------------------------------
         return ipList;
@@ -63,7 +77,7 @@ public class RadixTest {
                     i = Integer.parseInt(i.substring(2), 16) + "";
                 else if(i.length() == 8) // Binary Octet
                     i = Integer.parseInt(i, 2) + "";
-                else if(i.charAt(0) == '0' && Integer.parseInt(i) > 255)
+                else if(i.charAt(0) == '0' && i.length() == 4)
                     i = Integer.parseInt(i, 8) + "";
                 IP_OUT += (i + ".");
             }
