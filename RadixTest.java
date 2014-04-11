bb6cd2d0fa28441e30bd06e27041bfc05256845e
Striv <strivcini@gmail.com>
2014-04-11 00:50:11 -0400

    Corrections to String Parse

--- RadixTest.java
+++ RadixTest.java
@@ -13,7 +13,7 @@ public class RadixTest {
             // Hex Strings (Dot Separated)
             String re1 = "0x([0-9a-fA-F]{1,2})\\.0x([0-9a-fA-F]{1,2})\\.0x([0-9a-fA-F]{1,2})\\.0x([0-9a-fA-F]{1,2})";
             // Binary String (Dot Separated)
-            String re2 = "([0-1]{1,8})\\.([0-1]{1,8})\\.([0-1]{1,8})\\.([0-1]{1,8})";
+            String re2 = "([0-1]{4,8})\\.([0-1]{4,8})\\.([0-1]{4,8})\\.([0-1]{4,8})";
             // Octal String (Dot Separated)
             String re3 = "0[0-7]{3}\\.0[0-7]{3}\\.0[0-7]{3}\\.0[0-7]{3}";
             // Decimal Strings (Dot Separated)
@@ -53,7 +53,7 @@ public class RadixTest {
             {
                 String testIP = makeIP(in.substring(m.start(),m.end()));
                 //System.out.println("Testing: " + testIP);
-                System.out.println("PreFabString: " + in.substring(m.start(),m.end()));
+                System.out.println(in.substring(m.start(),m.end()) + "\t\t\t-->\t\t\t" + testIP);
                 if(validIP(testIP))
                 {
                     ipList.add(testIP);    
@@ -96,7 +96,7 @@ public class RadixTest {
             }
             else if(IP.length() == 31)
             {
-                IP_OUT += IP.substring(0,6) + ".";
+                IP_OUT += "0" + IP.substring(0,6) + ".";
                 IP_OUT += IP.substring(7,14) + ".";
                 IP_OUT += IP.substring(15,22) + ".";
                 IP_OUT += IP.substring(23,30);                
@@ -104,15 +104,15 @@ public class RadixTest {
             else if(IP.length() == 32)
             {
                 IP_OUT += IP.substring(0,7) + ".";
-                IP_OUT += IP.substring(7,14) + ".";
-                IP_OUT += IP.substring(15,22) + ".";
-                IP_OUT += IP.substring(23,30);                
+                IP_OUT += IP.substring(8,15) + ".";
+                IP_OUT += IP.substring(16,23) + ".";
+                IP_OUT += IP.substring(24,31);                
             }
             else if(IP.length() == 12)
             {
                 IP_OUT += "0" + IP.substring(0,2) + ".";
                 IP_OUT += "0" + IP.substring(3,5) + ".";
-                IP_OUT += "0" + IP.substring(6,7) + ".";
+                IP_OUT += "0" + IP.substring(6,8) + ".";
                 IP_OUT += "0" + IP.substring(9,11);
             }
             return makeIP(IP_OUT);
